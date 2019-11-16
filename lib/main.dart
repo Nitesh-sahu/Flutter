fc0b1888f675f1a73776dcf01dbd1dec134be8b5
Nitesh-sahu <ntssahu485@gmail.com>
2019-11-16 20:16:35 +0530

    SplashScren

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:splashscreen/LoginPage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: SplashScreen(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      fontFamily: "Arial",
+    ),
+
+//    routes: <String, WidgetBuilder>{
+//      '/SplashScreen': (BuildContext context) => new SplashScreen(),
+//      '/SecondPage': (BuildContext context) => new SecondPage(),
+//    },
+  ));
+}
+
+//class SecondPage extends StatefulWidget {
+//  @override
+//  _SecondPageState createState() => _SecondPageState();
+//}
+//
+//class _SecondPageState extends State<SecondPage> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      backgroundColor: Colors.redAccent,
+//      body: Container(
+//        height: MediaQuery.of(context).size.height,
+//        width: MediaQuery.of(context).size.width,
+//        child: Center(
+//          child: ListView(
+//            children: <Widget>[
+//              Image(
+//                image: AssetImage("image/NITESH.jpg"),
+//                height: 1000.0,
+//                width: 750.0,
+//              )
+//            ],
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var duration = new Duration(milliseconds: 1);
+    return new Timer(duration, navigation);
+  }
+  void navigation()
+  {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>LoginPage()));
+  }
+
+//  void navigationPage() {
+//    Navigator.of(context).pushNamed('/SecondPage');
+//  }
+
+  // ignore: must_call_super
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+//               image: NetworkImage("https://www.telegraph.co.uk/content/dam/technology/2016/05/11/new-instagram-logo_1_trans_NvBQzQNjv4BqqVzuuqpFlyLIwiB6NTmJwfSVWeZ_vEN7c6bHu2jJnT8.jpg?imwidth=1400"),
+//                    height: 40,
+//                    width: 40,
+                image: AssetImage("image/logo.png"),
+                height: 200,
+                width: 200,
+              ),
+              Text(
+                "BLOGPOST",
+                style: TextStyle(
+                  color: Colors.yellowAccent,
+                  fontSize: 20.5,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
